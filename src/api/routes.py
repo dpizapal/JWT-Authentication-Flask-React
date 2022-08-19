4e8e54ec3555fff08c1e92bf02b53fa2b64bef43
dpizapal <dpizapal@yahoo.es>
2022-08-19 11:05:37 +0000

    routes definidas

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,8 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
+import json
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +18,50 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# New USER [POST]
+
+@api.route('/user', methods = ['POST'])
+def create_new_user():
+    body = json.loads(request.data)
+    new_user = User(email=body["email"], password=body["password"], is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    response_body = {
+        "msg": ("usuario creado", new_user)
+    }
+    
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email = email).first()
+    if user is None: 
+        return jsonify({"msg": "User doesn't exist"}), 404
+    if email != user.email or password != user.password:
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    print(user)
+    access_token = create_access_token(identity = email)
+    return jsonify(access_token=access_token)  
+
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+
+@app.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
+
+    
+
+
